59bbef51320dfe44eaeda3e8e22b3dae30bf0a37
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-08-08 10:42:25 +0800

    t

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+     viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', 
+          rename: '404.html'
+        }
+      ]
+    })
+  ],
+ 
   base: 'facultyscheduler/',
   resolve: {
     alias: {
