a52e48420e8d6c0be8891ac18f10f9d8bb6fa696
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-11-13 20:34:13 +0800

    Updates

--- assets/FotgotPassword-bDJHOvdz.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Po53OIl0.js";function t(){return o.jsx("div",{children:"hello"})}export{t as default};
---
+++ assets/skeleton-C4B7ELaM.js
@@ -0,0 +1,6 @@
+import{c as o,j as r,i as c}from"./index-DBxbh4tE.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],t=o("loader-circle",s);function i({className:e,...a}){return r.jsx("div",{className:c("animate-pulse rounded-md bg-primary/10",e),...a})}export{t as L,i as S};
