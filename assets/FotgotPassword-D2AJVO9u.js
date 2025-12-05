3fda23a1955ddb8776a58a58fe16ef2c82029a17
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-12-06 04:17:48 +0800

    Updates

--- assets/FotgotPassword-D2AJVO9u.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DXcYVmw3.js";function t(){return o.jsx("div",{children:"hello"})}export{t as default};
--- assets/skeleton-CGPDtiiA.js
+++
@@ -1,1 +0,0 @@
-import{j as s,i as a}from"./index-DXcYVmw3.js";function t({className:e,...r}){return s.jsx("div",{className:a("animate-pulse rounded-md bg-primary/10",e),...r})}export{t as S};
